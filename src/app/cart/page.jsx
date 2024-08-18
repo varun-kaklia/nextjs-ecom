b5bc25d4cbf9f1b75914025e9987276ac5056e37
varunkaklia-moneyhop <varun.kaklia@moneyhop.co>
2024-08-18 18:06:51 +0530

    fixing some parts

--- src/app/cart/page.jsx
+++ src/app/cart/page.jsx
@@ -1,20 +1,22 @@
-"use client"
+"use client";
 import { cartState } from "@/store/cartState";
-import AddOn from "@/ui/addOn";
 import CustomButton from "@/ui/button";
 import Modal from "@/ui/modal";
 import { useRecoilState, useRecoilValue } from "recoil";
 import { useState } from "react";
-import { FaTimes, FaTrash } from "react-icons/fa";
+import { FaTimes } from "react-icons/fa";
 import { discount as discountApply } from "@/store/discount";
 import { coupons } from "@/constants/coupons";
+import CartItem from "@/components/CartItem";
+import { useRouter } from "next/navigation";
 
-const Cart = () => {
-  const [cart, setCart] = useRecoilState(cartState);
+const CartPage = () => {
+  const [cart,setCart] = useRecoilState(cartState);
   const [isCouponOpen, setCouponOpen] = useState(false);
   const [couponCode, setCouponCode] = useState("");
   const [appliedCoupon, setAppliedCoupon] = useState(null);
   const discount = useRecoilValue(discountApply);
+  const router = useRouter();
 
   const handleApplyCoupon = () => {
     if (coupons[couponCode]) {
@@ -28,18 +30,7 @@ const Cart = () => {
   const handleCancelCoupon = () => {
     setAppliedCoupon(null);
     setCouponOpen(false);
-  };
-
-  const handleQuantityChange = (id, quantity) => {
-    setCart((prevCart) =>
-      prevCart.map((item) =>
-        item.id === id ? { ...item, quantity: Math.max(1, quantity) } : item
-      )
-    );
-  };
-
-  const handleRemoveItem = (id) => {
-    setCart((prevCart) => prevCart.filter((item) => item.id !== id));
+    setCouponCode("");
   };
 
   const calculateSubtotal = () => {
@@ -60,11 +51,16 @@ const Cart = () => {
     return totalWithGST.toFixed(2);
   };
 
+  const handleCheckout = () => {
+    router.push("/successpage");
+    setCart([])
+  };
+
   return (
     <div className="p-4 bg-white w-full shadow-lg rounded-lg">
       <Modal
         isOpen={isCouponOpen}
-        onClose={() => setCouponOpen(false)}
+        onClose={() => handleCancelCoupon()}
         title="Apply Coupon"
       >
         <input
@@ -96,50 +92,7 @@ const Cart = () => {
       ) : (
         <div className="space-y-4">
           {cart.map((item) => (
-            <div key={item.id} className="flex items-center border-b pb-4 mb-4">
-              <img
-                src={item.image}
-                alt={item.productName}
-                className="w-24 h-24 object-cover rounded-md mr-4"
-              />
-              <div className="flex-grow">
-                <h3 className="text-lg font-semibold">{item.productName}</h3>
-                <p
-                  title={item?.productDescription}
-                  className="text-xs text-gray-600 break-words max-h-[15px] overflow-hidden"
-                >
-                  {item.productDescription}
-                </p>
-                <div className="flex items-center">
-                  <p className="text-gray-700 line-through">
-                    ${(item.price + item?.discount).toFixed(2)}
-                  </p>
-                  <p className="text-gray-700 font-semibold px-2">
-                    ${item.price.toFixed(2)}
-                  </p>
-                </div>
-                <div className="flex items-center mt-2 w-1/3">
-                  <AddOn quantity={item?.quantity} id={item?.id}>
-                    <input
-                      type="number"
-                      value={item.quantity}
-                      min="1"
-                      onChange={(e) =>
-                        handleQuantityChange(item.id, parseInt(e.target.value))
-                      }
-                      className="w-20 p-1 border text-center border-gray-300 rounded-md mx-2"
-                    />
-                  </AddOn>
-                  <button
-                    title="Remove Item"
-                    onClick={() => handleRemoveItem(item.id)}
-                    className="flex items-center justify-center w-8 h-8 bg-red-500 ml-2 text-white rounded-md hover:bg-red-600"
-                  >
-                    <FaTrash />
-                  </button>
-                </div>
-              </div>
-            </div>
+            <CartItem key={item.id} item={item} />
           ))}
           <div className="flex flex-col items-end">
             {appliedCoupon ? (
@@ -180,6 +133,7 @@ const Cart = () => {
             <CustomButton
               text={"Checkout"}
               customClass={"w-[20%] max-w-[30%] mt-4"}
+              onClickHandle={() => handleCheckout()}
             />
           </div>
         </div>
@@ -188,4 +142,4 @@ const Cart = () => {
   );
 };
 
-export default Cart;
+export default CartPage;
